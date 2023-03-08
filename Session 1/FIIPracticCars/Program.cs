2459d0cb9705a13c0c155af292caecf098b3036e
Cristian Stan <cristian.stan@expertnetwork.ro>
2023-03-08 13:09:10 +0200

    Renamed userVehicles to users.

--- Session 1/FIIPracticCars/Program.cs
+++ Session 1/FIIPracticCars/Program.cs
@@ -4,14 +4,14 @@ using Microsoft.EntityFrameworkCore;
 var carsContext = new CarsContext();
 
 
-var userVehicles = carsContext.Users
+var users = carsContext.Users
   .Include(u => u.Vehicles)
   .ThenInclude(v => v.Model)
   .ThenInclude(m => m.Brand)
   .ToList();
 
 Console.WriteLine("Results:");
-foreach (var user in userVehicles)
+foreach (var user in users)
 {
   Console.WriteLine($"[{user.Id}] {user.FirstName}, {user.LastName}");
   foreach (var vehicle in user.Vehicles)
